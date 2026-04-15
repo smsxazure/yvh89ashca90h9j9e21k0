004338cbdb253ee276bbd12d17c18b364a9bc4af
smsxazure <smsxazure@gmail.com>
2026-04-15 20:11:41 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+function goToMGR() {
+    localStorage.setItem("lastPage", "mgr-dashboard.html");
+    window.location.href = 'mgr-dashboard.html';
+}
+
+function goToACC() {
+    localStorage.setItem("lastPage", "acc-dashboard.html");
+    window.location.href = 'acc-dashboard.html';
+}
+
+function goToENG() {
+    localStorage.setItem("lastPage", "eng-dashboard.html");
+    window.location.href = 'eng-dashboard.html';
+}
+
+function goToGHK() {
+    localStorage.setItem("lastPage", "ghk-dashboard.html");
+    window.location.href = 'ghk-dashboard.html';
+}
+
+function goToPMO() {
+    localStorage.setItem("lastPage", "pmo-dashboard.html");
+    window.location.href = 'pmo-dashboard.html';
+}
+
+function goToRRD() {
+    localStorage.setItem("lastPage", "rrd-dashboard.html");
+    window.location.href = 'rrd-dashboard.html';
+}
+
+function goToSEC() {
+    localStorage.setItem("lastPage", "sec-dashboard.html");
+    window.location.href = 'sec-dashboard.html';
+}
